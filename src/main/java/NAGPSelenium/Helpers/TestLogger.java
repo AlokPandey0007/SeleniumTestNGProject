25795afbdc77c5e9494438311018dce232bdb696
AlokPandey0007 <59498778+AlokPandey0007@users.noreply.github.com>
2025-04-06 13:24:50 +0530

    Update TestLogger.java

--- src/main/java/NAGPSelenium/Helpers/TestLogger.java
+++ src/main/java/NAGPSelenium/Helpers/TestLogger.java
@@ -9,28 +9,28 @@ public class TestLogger {
 	private static final Logger logger = (Logger) LogManager.getLogger(TestLogger.class);
 
 	public static void info(String message) {
-		long threadId = Thread.currentThread().threadId();
+		long threadId = Thread.currentThread().getId();
 		logger.info("Thread " + threadId + ": " + message);
 
 	}
 
 	public static void error(String message) {
-		long threadId = Thread.currentThread().threadId();
+		long threadId = Thread.currentThread().getId();
 		logger.error("Thread " + threadId + ": " + message);
 	}
 
 	public static void debug(String message) {
-		long threadId = Thread.currentThread().threadId();
+		long threadId = Thread.currentThread().getId();
 		logger.debug("Thread " + threadId + ": " + message);
 	}
 
 	public static void warn(String message) {
-		long threadId = Thread.currentThread().threadId();
+		long threadId = Thread.currentThread().getId();
 		logger.warn("Thread " + threadId + ": " + message);
 	}
 
 	public static void trace(String message) {
-		long threadId = Thread.currentThread().threadId();
+		long threadId = Thread.currentThread().getId();
 		logger.trace("Thread " + threadId + ": " + message);
 	}
 
